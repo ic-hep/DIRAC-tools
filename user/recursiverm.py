b45dbc74fc1c767e1b72a734040ca2efefbf7430
Simon Fayer <sf105@ic.ac.uk>
2022-11-04 17:07:26 +0000

    py3 recursive rm

--- user/recursiverm.py
+++ user/recursiverm.py
@@ -22,16 +22,16 @@ class RecursiveRm(object):
 
   def clear_directory(self, directory_path, se_name, dry_run=True):
 
-    print "Clearing directory: %s" % directory_path
+    print("Clearing directory: %s" % directory_path)
     
     dir_content =  self.__rpcclient.listDirectory(directory_path, False)
 
     if not dir_content["OK"]:
-      print "Failed to contact DIRAC server for %s" % directory_path
+      print("Failed to contact DIRAC server for %s" % directory_path)
       return
     
     if directory_path in dir_content['Value']['Failed']:
-      print "Could not access %s, maybe it doesn't exist?" % directory_path
+      print("Could not access %s, maybe it doesn't exist?" % directory_path)
       return
 
     subdirs = dir_content['Value']['Successful'][directory_path]['SubDirs']
@@ -56,7 +56,7 @@ class RecursiveRm(object):
     dir_content =  self.__rpcclient.listDirectory(directory_path, False)
 
     if not dir_content["OK"]:
-      print  "Could not access %s" % directory_path
+      print( "Could not access %s" % directory_path)
       return False
 
 
@@ -75,22 +75,22 @@ class RecursiveRm(object):
   
     res = self.__rpcclient.getReplicas(filename, False)
     if not res["OK"]:
-      print "Could not get replica status for %s" % filename
+      print("Could not get replica status for %s" % filename)
       return False
   
     ses = res['Value']['Successful'][filename].keys()
     
     # remove file regardless of number of replicas
     if se_name == "Any":
-      print "%s" % filename
+      print("%s" % filename)
       if not dry_run:
         deleted = self.__dm.removeFile(filename)
         if not deleted["OK"]:
-          print "Function call to removeFile failed, file not deleted: %s" \
-                                                            % str(deleted)
+          print("Function call to removeFile failed, file not deleted: %s" \
+                                                            % str(deleted))
           return False
         elif not deleted["Value"]["Successful"]:
-          print "Failed to delete file: %s" % str(deleted)
+          print("Failed to delete file: %s" % str(deleted))
           return False
 
       return True
@@ -98,29 +98,29 @@ class RecursiveRm(object):
     # file exists only at the chosen SE
     #     -> delete file and remove from file catalogue
     if len(ses) == 1 and se_name in ses:
-      print "%s" % filename
+      print("%s" % filename)
       if not dry_run:
         deleted = self.__dm.removeFile(filename)
         if not deleted["OK"]:
-          print "Function call to removeFile failed, file not deleted: %s" \
-                                                            % str(deleted)
+          print("Function call to removeFile failed, file not deleted: %s" \
+                                                            % str(deleted))
           return False
         elif not deleted["Value"]["Successful"]:
-          print "Failed to delete file: %s" % str(deleted)
+          print("Failed to delete file: %s" % str(deleted))
           return False
       return True
         
     # file exists at the chosen SE and elswhere -> delete replica at chosen SE
     if len(ses) > 1 and se_name in ses:
-      print "%s" % filename
+      print("%s" % filename)
       if not dry_run:
         deleted = self.__dm.removeReplica(se_name, filename)
         if not deleted["OK"]:
-          print "Function call to removeReplica failed, replica not deleted: %s" \
-                                                                  % str(deleted)
+          print("Function call to removeReplica failed, replica not deleted: %s" \
+                                                                  % str(deleted))
           return False
         elif not deleted["Value"]["Successful"]:
-          print "Failed to delete replica: %s" % str(deleted)
+          print("Failed to delete replica: %s" % str(deleted))
           return False
 
 
@@ -130,11 +130,11 @@ class RecursiveRm(object):
      
 
   def print_stats(self):
-    print ""
-    print "Number of files deleted: %s" % self.__n_files
-    print "NUmber of (sub)directories deleted: %s" % self.__n_dirs
+    print("")
+    print("Number of files deleted: %s" % self.__n_files)
+    print("Number of (sub)directories deleted: %s" % self.__n_dirs)
     space = self.__space_freed/(1024.0 * 1024.0 * 1024.0)
-    print "Space freed: %0.3f GB" % space
+    print("Space freed: %0.3f GB" % space)
 
  
 def main():
@@ -144,23 +144,23 @@ def main():
   opts, args = getopt.getopt(sys.argv[1:], "n")  
 
   if len(args) < 2:
-    print "Usage: recursiverm.py [-n] <SE> <LFN> [<LFN>...]"
-    print "       -n: dryRun (list files to be deleted)"
-    print "Use 'Any' for <SE> to delete all replicas in a directory"
-    print "Example: recursiverm.py -n UKI-LT2-IC-HEP-disk /vo.londongrid.ac.uk/test2"
+    print("Usage: recursiverm.py [-n] <SE> <LFN> [<LFN>...]")
+    print("       -n: dryRun (list files to be deleted)")
+    print("Use 'Any' for <SE> to delete all replicas in a directory")
+    print("Example: recursiverm.py -n UKI-LT2-IC-HEP-disk /vo.londongrid.ac.uk/test2")
     sys.exit(1)
 
   proxy_info = getProxyInfo()   
   if not "VOMS" in proxy_info["Value"] or not proxy_info["Value"]["VOMS"]:
-    print "Error: Your proxy does not contain a VOMS signature."
-    print "(Try using dirac-proxy-init -g [voname]_user -M)"
+    print("Error: Your proxy does not contain a VOMS signature.")
+    print("(Try using dirac-proxy-init -g [voname]_user -M)")
     sys.exit(2)
 
 
   for opt, _ in opts:
     if opt == "-n":
       dry_run = True
-      print "dryRun only: list files to be deleted"
+      print("dryRun only: list files to be deleted")
 
   rrm = RecursiveRm()
 
@@ -168,8 +168,8 @@ def main():
   for tpath in args[1:]:
     tpath = os.path.normpath(tpath)
     if len(tpath.split("/")) < 3:
-      print "Sorry, I can't let you do that Dave: Path %s is too broad." \
-                                                                    % tpath
+      print("Sorry, I can't let you do that Dave: Path %s is too broad." \
+                                                                    % tpath)
       sys.exit(3)
     paths.append(tpath)
 
